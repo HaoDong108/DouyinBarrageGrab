86152d9b73a9cb1e0730d81c571136275efb4a99
小白猿 <1083092844@qq.com>
2023-03-04 14:52:58 +0800

    去除冗余代码

--- BarrageGrab/Proxy/SystemProxy.cs
+++ BarrageGrab/Proxy/SystemProxy.cs
@@ -53,35 +53,6 @@ namespace BarrageGrab.Proxy
             return $"<{processID}>";
         }
 
-        // 生成自签名证书
-        protected X509Certificate2 EnsureCertificate(string subjectName)
-        {
-            // 添加证书到证书存储
-            var store = new X509Store(StoreName.Root, StoreLocation.LocalMachine);
-            store.Open(OpenFlags.ReadWrite);
-            //判断证书是否已经存在
-            var certificates = store.Certificates.Find(X509FindType.FindBySubjectName, subjectName, false);
-            if (certificates.Count > 0)
-            {
-                return certificates[0];
-            }
-
-            // 创建证书请求
-            var request = new CertificateRequest($"CN={subjectName}", RSA.Create(2048), HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-
-            // 添加扩展属性
-            request.CertificateExtensions.Add(new X509BasicConstraintsExtension(false, false, 0, false));
-            request.CertificateExtensions.Add(new X509KeyUsageExtension(X509KeyUsageFlags.KeyEncipherment | X509KeyUsageFlags.DigitalSignature, false));
-            request.CertificateExtensions.Add(new X509SubjectKeyIdentifierExtension(request.PublicKey, false));
-
-            // 用自己的私钥签名并生成证书
-            var certificate = request.CreateSelfSigned(DateTimeOffset.Now.AddDays(-1), DateTimeOffset.Now.AddDays(365));
-            store.Add(certificate);
-            store.Close();
-
-            return certificate;
-        }
-
         //注册系统代理
         protected void RegisterSystemProxy()
         {
