199dd5d3894123067821cfba6d678c53f5ebd643
小白猿 <1083092844@qq.com>
2023-08-17 20:32:18 +0800

    迁移实体文件位置

---
+++ BarrageGrab/Modles/JsonEntity/Command.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarrageGrab.Modles.JsonEntity
+{
+    public enum CommandCode
+    {
+        /// <summary>
+        /// 空指令
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// 安全关闭程序
+        /// </summary>
+        Close = 1,
+    }
+
+    public class Command
+    {
+        /// <summary>
+        /// 指令标识
+        /// </summary>
+        public CommandCode Cmd { get; set; }
+
+        /// <summary>
+        /// 附加数据
+        /// </summary>
+        public object Data { get; set; }
+    }
+}
