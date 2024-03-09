4e0a87183355e2882082f0dc5e1292f1fc7752d2
Muihd <1083092844@qq.com>
2024-03-10 03:41:14 +0800

    支持 自动暂停页面播放

--- BarrageGrab/Scripts/inject/livePage.js
+++ BarrageGrab/Scripts/inject/livePage.js
@@ -35,3 +35,26 @@ targetElement.addEventListener('keydown', event => {
 WebSocket.prototype.close = function () { return; }
 
 
+/*判断是否自动关闭 */
+//获取路由参数
+function getQueryString(name) {
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    let r = window.location.search.substr(1).match(reg); //search,查询？后面的参数，并匹配正则
+    if (r != null) return unescape(r[2]);
+    return null;
+}
+
+if (getQueryString("pause") === "true" || AUTOPAUSE === true) {
+    //自动暂停一次播放
+    var pauseIdx = setInterval(() => {
+        let playbtn = document.querySelector(".xgplayer-play");
+        if (!playbtn) return;
+        let status = playbtn.getAttribute("data-state");
+        if (status !== "play") return;
+        playbtn.click();
+        clearInterval(pauseIdx);
+        console.log("已自动暂停播放");
+    }, 500);
+}
+
+
